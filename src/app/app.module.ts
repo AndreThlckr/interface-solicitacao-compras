d8ccd7fd6b2773dd64bfd5a990d41934c9f72f3f
AndreThlckr <andretheilacker@gmail.com>
2019-06-29 15:20:02 -0300

    Modulos criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SolicitacaoComponent } from './solicitacao/solicitacao.component';
+import { AprovacaoComponent } from './aprovacao/aprovacao.component';
+import { RegistrosComponent } from './registros/registros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SolicitacaoComponent,
+    AprovacaoComponent,
+    RegistrosComponent
   ],
   imports: [
     BrowserModule,
