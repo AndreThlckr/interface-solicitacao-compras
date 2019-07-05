3cc6e482d005e2db0e743ee5d760be67b0c26ac4
AndreThlckr <andretheilacker@gmail.com>
2019-07-05 01:41:36 -0300

    Componente de solicitacao incrementado

--- src/app/solicitacao/solicitacao.component.ts
+++ src/app/solicitacao/solicitacao.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Solicitacao } from '../solicitacao';
 
 @Component({
   selector: 'app-solicitacao',
@@ -10,16 +11,45 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SolicitacaoComponent implements OnInit {
 
-  @Input() solicitacaoDetails = { nome: '', descricao: '', preco: 0 }
+  form: FormGroup;
+  solicitacaoDetails: any;
+  formSubmitted = false;
+
+
+
+  //@Input() solicitacaoDetails = { nome: '', descricao: '', preco: 0 }
 
   constructor(
-    public dataService: DataService, 
+    private formBuilder: FormBuilder,
+    public dataService: DataService,
     public router: Router
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.buildForm();
+  }
 
-  addSolicitacao(dataEmployee) {
+  buildForm() {
+    this.form = this.formBuilder.group({
+      nome: [null, [Validators.required]],
+      descricao: [null, [Validators.required]],
+      preco: [null, [Validators.required]],
+    });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.formSubmitted = true;
+
+    if (this.form.valid) {
+      const result: Solicitacao = Object.assign({}, this.form.value);
+      this.dataService.createSolicitacao(result).subscribe((data: {}) => {
+        this.router.navigate([''])
+      })
+    }
+  }
+
+  addSolicitacao() {
     this.dataService.createSolicitacao(this.solicitacaoDetails).subscribe((data: {}) => {
       this.router.navigate([''])
     })
