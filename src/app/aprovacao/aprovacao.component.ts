8df5d9be09bd7d7f82897d147aaefc84da1e5fd5
AndreThlckr <andretheilacker@gmail.com>
2019-07-05 15:15:03 -0300

    Modulo de aprovacao finalizado

--- src/app/aprovacao/aprovacao.component.ts
+++ src/app/aprovacao/aprovacao.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Solicitacao } from '../solicitacao';
 
 @Component({
   selector: 'app-aprovacao',
@@ -10,26 +12,86 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AprovacaoComponent implements OnInit {
 
   id = this.actRoute.snapshot.params['id'];
-  solicitacaoData: any = {};
+  solicitacao: Solicitacao;
+  form: FormGroup;
+  formSubmitted = false;
+  solicitacaoNegada = false;
 
   constructor(
+    private formBuilder: FormBuilder,
     public dataService: DataService,
     public actRoute: ActivatedRoute,
     public router: Router
-  ) { 
+  ) {
   }
 
-  ngOnInit() { 
-    this.dataService.getSolicitacao(this.id).subscribe((data: {}) => {
-      this.solicitacaoData = data;
-    })
+  ngOnInit() {
+    this.buildForm();
+    this.setSituacaoValidators();
+
+    this.dataService.getSolicitacao(this.id)
+      .subscribe(data => {
+        this.form.patchValue(data);
+      });
   }
 
-  updateSolicitacao() {
-      this.dataService.updateSolicitacao(this.id, this.solicitacaoData).subscribe(data => {
+  buildForm() {
+    this.form = this.formBuilder.group({
+      nome: [{
+        value: null,
+        disabled: true
+      }],
+      descricao: [{
+        value: null,
+        disabled: true
+      }],
+      preco: [{
+        value: null,
+        disabled: true
+      }],
+      situacao: [null, [Validators.required]],
+      observacao: [{
+        value: null,
+        disabled: true
+      }],
+    });
+  }
+
+  setSituacaoValidators() {
+    const observacaoControl = this.form.get('observacao');
+
+    this.form.get('situacao').valueChanges
+      .subscribe(situacao => {
+
+        if (situacao === 'true') {
+          observacaoControl.clearValidators();
+          observacaoControl.setValue(null);
+          observacaoControl.disable();
+          this.solicitacaoNegada = false;
+        }
+
+        if (situacao === 'false') {
+          observacaoControl.enable();
+          observacaoControl.setValidators([Validators.required, Validators.minLength(5)]);
+          this.solicitacaoNegada = true;
+        }
+
+        observacaoControl.updateValueAndValidity();
+      });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.formSubmitted = true;
+    console.log('submit');
+
+    if (this.form.valid) {
+      console.log('valido');
+      //copia os dados do formulário para 'result' e cria solicitacao
+      const result: Solicitacao = Object.assign({}, this.form.getRawValue());
+      this.dataService.updateSolicitacao(this.id, result).subscribe((data: {}) => {
         this.router.navigate(['/lista-de-espera'])
       })
-    
+    }
   }
-
 }
